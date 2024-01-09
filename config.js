330c0a6aa63ec8ead9c7f1e998071ebfb481edfe
DARKOFC009 <84442326+nikhilarma@users.noreply.github.com>
2024-01-09 15:27:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['91975506270', 'nik', true],
+  ['917067432198', 'nik', true], 
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['917067432198','91975506270'] 
+global.prems = ['917067432198', '91975506270', '917067432198']
+global.allowed = ['917067432198', '91975506270', '917067432198']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -50,17 +50,17 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = '🖤⏤͟͟͞͞ ×͜× ᴄ͢͢͢ʀɪᴍɪɴᴀʟ࿐'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
-global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
-global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
+global.packname = '🖤⏤͟͟͞͞ ×͜× ᴄ͢͢͢ʀɪᴍɪɴᴀʟ࿐' 
+global.author = '🅼eow💖' 
+global.menuvid = 'https://telegra.ph/file/6adcefabca0b7ee6d3dca.mp4'
+global.igfg = '..' 
+global.dygp = '. '
+global.fgsc = '. ' 
+global.fgyt = '. '
+global.fgpyp = '.'
+global.fglog = '.' 
 global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
 
